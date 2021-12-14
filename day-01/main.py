634ce12f0c5c05a445ad41f5f4a8e1a8d52710df
Dave Brown <dave.brown@exit107.com>
2021-12-14 00:00:29 -0700

    Part two of day one

--- day-01/main.py
+++ day-01/main.py
@@ -1,27 +1,32 @@
 import os
 import sys
 
+window = 3
 overall = 0
 inc = 0
 dec = 0
 nop = 0
 with open(sys.argv[1], 'r') as input_fh:
-    for idx, x in enumerate(input_fh.readlines()):
-        if idx == 0:
-            prev = x
-        if x > prev:
-            delta = 'increased'
-            inc +=1
-            overall += 1
-        if x < prev:
-            delta = 'decreased'
-            dec += 1
-            overall -= 1
-        if x == prev:
-            delta = 'no change'
-            nop += 1
-        print(f'{x} ({delta}:{overall})')
-        prev = x
+    measurements = input_fh.readlines()
+
+measurements = [int(x.strip()) for x in measurements]
+for idx in range(len(measurements)):
+    cur = sum(measurements[idx:idx + window])
+    if idx == 0:
+        prev = cur
+    if cur > prev:
+        delta = 'increased'
+        inc +=1
+        overall += 1
+    if cur < prev:
+        delta = 'decreased'
+        dec += 1
+        overall -= 1
+    if cur == prev:
+        delta = 'no change'
+        nop += 1
+    print(f'{cur} ({delta}:{overall})')
+    prev = cur
 
 print(f'Overall inc: {inc}')
 print(f'Overall dec: {dec}')
