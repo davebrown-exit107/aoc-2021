e7f40c0cc43dde551c8b76fe58c07657d63bc668
Dave Brown <dave.brown@exit107.com>
2021-12-14 23:22:11 -0700

    Seems like pt 2 should be done, but I'm getting the wrong answer... Headed out to consult reddit I guess

--- day-04/main.py
+++ day-04/main.py
@@ -69,6 +69,7 @@ def play_bingo(boards: list, call_sequence: list) -> (int, int):
 
 winning_board_idx, winning_step = play_bingo(boards, call_sequence)
 print(f'board {winning_board_idx} wins on step {winning_step}')
+pprint(boards[winning_board_idx])
 score = calculate_score(boards[winning_board_idx], winning_step)
 print(f'winning board score: {score}')
 
@@ -76,5 +77,30 @@ print(f'winning board score: {score}')
 # Part 2 #
 ##########
 print('#'*25)
-print('Part 2')
+print('Part 2: Admitting defeat')
 print('#'*25)
+
+def lose_bingo(boards: list, call_sequence: list) -> (list, int):
+    '''
+    Walk through the call_sequence and mark boards, if a bingo is
+    found check to see if it is the last board to have a bingo,
+    then return the board and the step in the call sequence.
+    '''
+    for step, num in enumerate(call_sequence):
+        for w, board in enumerate(boards):
+            for x, row in enumerate(board):
+                for y, col in enumerate(row):
+                    if col == num:
+                        boards[w][x][y] = 'X'
+                        if check_board(boards[w]):
+                            winner = boards.pop(w)
+                            if len(boards) == 0:
+                                return winner, step
+
+
+winning_board, winning_step = lose_bingo(boards, call_sequence)
+print(f'last winning board wins on step {winning_step}')
+pprint(winning_board)
+score = calculate_score(winning_board, winning_step)
+print(f'winning board score: {score}')
+
